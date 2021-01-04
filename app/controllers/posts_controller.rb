4c01a8d98b85ff5bbb676799a40b07cf00b3c0bd
haruyamu <haruya.20020331@icloud.com>
2021-01-04 18:05:54 +0900

    user edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,42 @@
 class PostsController < ApplicationController
+  before_action :set_params, only: %i[ edit update show destory]
   def index
-  end
-
-  def edit
-  end
-
-  def show
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
+    @post = Post.new
   end
+  def create
+    @post = Post.new(create_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  def show
+  end
+  
+
+  private
+  def create_params
+    params.require(:post).permit(:title,:place,:content,:image).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @post = Post.find(params[:id])
+  end
+  
+  
 end
